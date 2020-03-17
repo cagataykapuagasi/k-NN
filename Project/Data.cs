d2de18520b9c2b6b74e95e4427f29daa1c437167
Cagatay <cagatay.kapuagasi@gmail.com>
2020-03-18 01:04:30 +0300

    Read data &Tokenizer & stop-words

--- Project/Data.cs
+++ Project/Data.cs
@@ -1,10 +1,84 @@
 ﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Globalization;
+
 namespace Project
 {
     public class Data
     {
+        List<string[]> positive = new List<string[]>();
+        List<string[]> negative = new List<string[]>();
+        List<string[]> neutral = new List<string[]>();
+        List<string> stop_words = new List<string>();
+
         public Data()
         {
+            init();
+        }
+
+
+        void init()
+        {
+
+            try //klasördeki verilerin okunması source: https://stackoverflow.com/questions/5840443/how-to-read-all-files-inside-particular-folder
+            {
+                stop_words.AddRange(File.ReadLines("stop-words.txt"));
+
+                foreach (string file in Directory.EnumerateFiles("1"))
+                {
+                    var tokenizedStrings = tokenizer(File.ReadAllText(file).ToLower(new CultureInfo("tr-TR", false)));
+                    var stopWordedStrings = stopWords(tokenizedStrings);
+                    positive.Add(stopWordedStrings);
+                    
+                }
+                foreach (string file in Directory.EnumerateFiles("2"))
+                {
+                    var tokenizedStrings = tokenizer(File.ReadAllText(file).ToLower(new CultureInfo("tr-TR", false)));
+                    var stopWordedStrings = stopWords(tokenizedStrings);
+                    negative.Add(stopWordedStrings);
+                }
+                foreach (string file in Directory.EnumerateFiles("3"))
+                {
+                    var tokenizedStrings = tokenizer(File.ReadAllText(file).ToLower(new CultureInfo("tr-TR", false)));
+                    var stopWordedStrings = stopWords(tokenizedStrings);
+                    neutral.Add(stopWordedStrings);
+                }
+
+                foreach (String i in positive[5])
+                    Console.WriteLine(i);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Hata: " + e.Message);
+
+                Environment.Exit(0);  //hata olma durumunda işlemlerin devam etmemesi için
+            }
+        }
+
+
+        string[] tokenizer(String value)
+        {
+            return value.Split(null); 
+        }
+
+        string[] stopWords(String[] array)
+        {
+            List<string> newArray = new List<string>(array);
+
+            foreach (string a in array)
+            {
+                foreach(string s in stop_words)
+                {
+                    if(a == s)
+                    {
+                        newArray.Remove(a);
+                        break;
+                    }
+                }
+            }
+
+            return newArray.ToArray();
         }
     }
 }
--- Project/Program.cs
+++ Project/Program.cs
@@ -4,8 +4,12 @@ namespace Project
 {
     class Program
     {
+        
+
         static void Main(string[] args)
         {
+            Data data = new Data();
+
             Console.WriteLine("Hello World!");
         }
     }
