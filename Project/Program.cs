43c73c94d0efb7daa412de861225a9936fe3eb33
cagataykapuagasi <cagatay.kapuagasi@gmail.com>
2020-03-19 02:10:22 +0300

    Final

--- Project/Program.cs
+++ Project/Program.cs
@@ -11,8 +11,12 @@ namespace Project
         static void Main(string[] args)
         {
             Data data = new Data();
-
             Console.WriteLine("Hello World!");
+
+            foreach(Object i in data.list)
+            {
+                //Console.WriteLine(i.name);
+            }
 
             Console.Read();
         }
