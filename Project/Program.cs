f48326703723d215aa0712d37b116fe0bda9d4bb
cagataykapuagasi <cagatay.kapuagasi@gmail.com>
2020-03-18 18:59:44 +0300

    Fix console

--- Project/Program.cs
+++ Project/Program.cs
@@ -1,16 +1,21 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Project
 {
     class Program
     {
-        
-
         static void Main(string[] args)
         {
             Data data = new Data();
 
             Console.WriteLine("Hello World!");
+
+            Console.Read();
         }
     }
 }
+
