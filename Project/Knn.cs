9d5023e6bbd71558a10a5e54ba7471885090b7b4
cagataykapuagasi <cagatay.kapuagasi@gmail.com>
2020-03-19 21:11:37 +0300

    Knn initial

---
+++ Project/Knn.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project
+{
+    class Knn
+    {
+        int k = 0;
+
+        public Knn(List<Object> list)
+        {
+            init(list);
+        }
+
+        void init(List<Object> list)
+        {
+            
+        }
+    }
+}
